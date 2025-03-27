88aaab1c85946a54ea654e12e19db92b0810af4f
Wagner Sabor <wsaborjr@gmail.com>
2025-03-27 15:41:42 -0300

    aula 27/03/2025

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View, Image } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={require("../assets/images/icon.png")} />
+    </View>
+  );
+}
